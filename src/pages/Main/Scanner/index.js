861d1b16d93b2c56f7ddfcde621a22e4a1f598d6
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-02-21 21:51:34 -0300

    Implementacao da area do leitor

---
+++ src/pages/Main/Scanner/index.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+
+import Quagga from 'quagga';
+
+import { validateIsbn } from '../../../services/books';
+
+import { Video, Container, ScanMarker } from './styles';
+
+function Scanner() {
+  let scannerAttemps = 0;
+  const onDetected = (result) => {
+    Quagga.offDetected(onDetected);
+    const isbn = result.codeResult.code;
+    if (validateIsbn(isbn)) {
+      alert(`ISBN válido`, isbn);
+    } else if (scannerAttemps >= 5) {
+      alert('Não é possível ler o código do livro');
+    }
+
+    scannerAttemps += 1;
+    Quagga.onDetected(onDetected);
+  };
+
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      Quagga.init(
+        {
+          inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: document.querySelector('#video'),
+            constraints: {
+              facingMode: 'environment',
+            }, // Or '#yourElement' (optional)
+          },
+          numOfWorkers: 1,
+          locate: true,
+          decoder: {
+            readers: ['ean_reader'],
+          },
+        },
+        (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('Inicialização completa. Pronto para começar');
+          Quagga.start();
+        },
+        Quagga.onDetected(onDetected)
+      );
+    }
+  }, []);
+  return (
+    <>
+      <Video id="video" />
+      <Container>
+        <ScanMarker>
+          <img
+            src="../../../assets/images/qr-scan-0.svg"
+            alt="Marca para leitura de código"
+            width="260"
+            height="260"
+          />
+          <p className="label">Aponte para o código de barras do livro</p>
+        </ScanMarker>
+      </Container>
+    </>
+  );
+}
+
+export default Scanner;
